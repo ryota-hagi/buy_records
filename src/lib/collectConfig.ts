54c4cd58aa827e4a71fce7f9cc2ea86dd1b4d3d7
ryota-hagi <ryota_h@arigat.co.jp>
2025-05-22 23:33:57 +0900

    Document collector API usage

---
+++ src/lib/collectConfig.ts
@@ -0,0 +1,6 @@
+import { CollectTask } from './collector';
+
+export const collectTasks: CollectTask[] = [
+  { platform: 'ebay', keyword: 'vinyl record', limit: 5 },
+  { platform: 'ebay', keyword: 'jazz record', limit: 5 },
+];
